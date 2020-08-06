b9cf9cde739d179b944f1e07d0ba7d2b87b3ad20
Tony Kwang Hyun Kim <tony.kim925@gmail.com>
2020-08-06 00:28:31 -0700

    Updated run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -5,10 +5,11 @@ library(dplyr)
 url <- "https://d396qusza40orc.cloudfront.net/
 getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-download.file(url, destfile = "./finalprojectdata/accelerometers.zip")
+download.file(url, destfile = "accelerometers.zip")
 unzip("accelerometers.zip")
 
-setwd("./finalprojetdata/UCI HAR Dataset")
+setwd("UCI Har Dataset")
+list.files()
 
 # Getting test data 
 x_test <- read.table("./test/X_test.txt") #str of 2947 obs of 561 variables
@@ -22,7 +23,7 @@ sub_train <- read.table("./train/subject_train.txt") #str of 7352 obs of 1 varia
 
 # Getting features(variable names) and activity labels 
 variable_names <- read.table("./features.txt") 
-activity_labels <- read.table("./activity_labels.txt") 
+activity_labels <- read.table("./activity_labels.txt")
 
 # Combining test and train datasets
 x_total <- rbind(x_test, x_train)
@@ -31,36 +32,45 @@ sub_total <- rbind(sub_test, sub_train)
 
 # Setting column names with correct variable names  
 colnames(x_total) <- variable_names[,2]
-
-# Setting up label dataset by substituting numerical values with activity labels
-label_total_renamed <- merge(label_total, activity_labels, by.x = "V1", 
-                             by.y = "V1", sort = FALSE)
-label_total_renamed <- rename(label_total_renamed, activitylabels = V2)[2]
-
-#Setting appropriate column name to sub_total
-sub_total <- rename(sub_total, subjectname = V1)
+colnames(label_total) <- "activityid"
+colnames(sub_total) <- "subjectid"
+colnames(activity_labels) <- c("activityid", "activitylabel")
 
 #combining main data set, subject name, and activity names
-combined_data <- cbind(sub_total, label_total_renamed, x_total)
+combined_data <- cbind(sub_total, label_total, x_total)
 View(combined_data)
 
 # Data has now been properly merged, descriptive activity names have been set 
 # and appropriate labels have been established. Now I will extract the measurements 
 # of mean and standard deviation for each measurement 
-
-columns <- colnames(combined_data)
-column_selected <- (grepl("subjectname", columns) | grepl("activitylabels", columns) |
-                    grepl("mean", columns) | grepl("std", columns) )
+column_selected <- grepl("*mean\\(\\)|*std\\(\\)|activityid|subjectid", 
+                         names(combined_data))
 mean_std_only <- combined_data[,column_selected]
+
+# Setting up label dataset by substituting numerical values with activity labels
+CleanData <- merge(mean_std_only, activity_labels, by = "activityid")
+CleanData <- CleanData[,c(2,69,3:68)]
 
 # Creating a second, independent tidy data set with average of each variable by
 # activity and each subject
 
-tidydata <- aggregate(.~subjectname + activitylabels, data = mean_std_only, mean)
-tidydata <- arrange(tidydata, subjectname, activitylabels)
+tidydata <- aggregate(. ~subjectid+activitylabel, CleanData, mean)
+tidydata <- arrange(tidydata, subjectid)
 
+View(tidydata)
+View(mean_std_only)
 #saving the tidydata for github 
 write.table(tidydata, file = "tidydata.txt", row.name = F )
+
+
+
+
+
+
+
+
+
+
 
 
 
